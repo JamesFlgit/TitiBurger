8346de27c355e27ed65ac5db80e77160fbda8b8c
James FLEURIE <james.fleurie@gmail.com>
2025-01-08 08:45:10 +0100

    Finalisation des pages HTML et CSS

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Sélectionnez les éléments
+const linkRubrique = document.querySelector('.link-rubrique');
+const rubriqueUl = document.querySelector('.link-rubrique__ul');
+
+// Fonction pour afficher la liste
+function afficherListe() {
+    rubriqueUl.style.visibility = 'visible';
+}
+
+// Fonction pour cacher la liste
+function cacherListe() {
+    rubriqueUl.style.visibility = 'hidden';
+}
+
+// Ajoutez les événements pour le survol et la sortie du survol
+linkRubrique.addEventListener('mouseover', afficherListe);
+linkRubrique.addEventListener('mouseout', cacherListe);
